31c5330eeac772f3689b101565d6d708ec267cb1
Eugene <me.eugene.south@gmail.com>
2024-11-25 06:52:02 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+import UiButton from "./components/UiButton.vue";
+
+export default {
+  components: {UiButton},
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="card">
+      <h1>UI Button</h1>
+      <ui-button
+          :text="'Add'"
+          :variant="'black'"
+          :is-active="true"
+          :is-loading="false"
+          :is-stretch="true"
+          :size="'xs'"
+      >
+        <!--  Можно ли сделать при передаче пустой строки ``,
+              чтобы размер по дефолту был md, в пропсе
+               default не работает. Или в разработке не будут передавать
+               пустые строки, вместо параметра?-->
+        <template #left v-if="true" > <!-- Надо ли связывать с isLoading -->
+<!--          <img class="icon" src="https://play.vuejs.org/logo.svg" alt="">-->
+<!--           Сделал костыль, через i
+               (Сюда можно, через копи-пасту
+               вставить иконку 🔍, вместо Х)
+               Не разобрался, как с выбором размера кноки,
+               изменялась и иконка через img-->
+          <i class="icon">X</i>
+        </template>
+<!--        Смотря где нужна иконка слева или справа   -->
+        <template #right v-if="false">
+          <i class="icon">X</i>
+        </template>
+
+      </ui-button>
+    </div>
+</template>
+
+<style scoped>
+
+.icon {
+  margin: 0 8px;
+}
+</style>
---
+++ src/components/UiButton.vue
@@ -0,0 +1,114 @@
+<template>
+  <button
+    :disabled="!isActive"
+    :class="[buttonSize, variant]"
+    :style="{width: isStretch? '100%': '',}"
+  >
+    <slot name="left"  />
+    {{isLoading? '' : text}}
+    <slot name="right" />
+    <span v-show="isLoading" class="loader"></span>
+  </button>
+</template>
+
+<script>
+export default {
+  props:{
+    text: String,
+    variant:{
+      type: String,
+      default: 'gray',
+      validate(value){
+        return ['', 'black', 'gray', 'red']
+      }
+    },
+    isActive: Boolean,
+    isLoading: Boolean,
+    isStretch: Boolean,
+    size: {
+      type: String,
+      default: 'md',
+      validator(value) {
+        return ['xs','md','lg'].includes(value)
+      }
+    }
+  },
+  computed:{
+    buttonSize(){
+      return{
+        xs: this.size==='xs',
+        md: this.size==='md',
+        lg: this.size==='lg',
+      }
+    }
+  }
+}
+</script>
+<!--Не понял, как до ума одной строчко довести disabled,
+ у меня при блокировке цвета с класса black,red,gray
+превосходствуют над цветовыми disabled, хотя cursor меняеяется -->
+<style  scoped>
+
+button.black:disabled,
+button.gray:disabled,
+button.red:disabled
+{
+  opacity: 0.3;
+  cursor: default;
+}
+button{
+  border-radius: 12px;
+  cursor: pointer;
+  align-items: center;
+  display: inline-flex;
+  justify-content: center;
+}
+button.xs{
+  padding: 8px 16px;
+  font-size: 12px;
+  line-height: 1.2;
+}
+button.md{
+  padding: 12px 20px;
+  font-size: 16px;
+  line-height: 1.5;
+}
+button.lg{
+  padding: 16px 28px;
+  font-size: 20px;
+  line-height: 1.8;
+}
+
+button.black{
+  background-color: #1e1e2f;
+  color: white;
+}
+button.red{
+  background-color: red;
+  color: white;
+}
+button.gray{
+  background-color: #A3A3B5;
+  color: black;
+}
+button span{
+  margin: 0 5px;
+  display: flex;
+}
+
+.loader {
+  margin: 0 auto;
+  width: 16px;
+  height: 16px;
+  border: 3px solid rgba(255, 255, 255, 0.3);
+  border-top: 3px solid white;
+  border-radius: 50%;
+  animation: spin 0.8s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
